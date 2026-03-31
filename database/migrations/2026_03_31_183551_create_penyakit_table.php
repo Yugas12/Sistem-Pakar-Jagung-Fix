a03c733ff31928a82e75f3a533e76b6c2a244850
Lutfi Hakim <lutfihakim1606@gmail.com>
2026-03-31 19:13:47 +0700

    fix migration penyakit

--- database/migrations/2026_03_31_183551_create_penyakit_table.php
+++ database/migrations/2026_03_31_183551_create_penyakit_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('penyakit', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('kode')->unique();   // P1, P2, dst
+            $table->string('nama');
+            $table->text('deskripsi');
+            $table->text('solusi');
         });
     }
 
